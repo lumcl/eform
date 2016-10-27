774ed21f9f0c0360e88bbd107821f607c95c4cbc
lum <ibmt20@hotmail.com>
2016-10-27 15:41:16 +0800

    Changes

--- app/models/sap/csks.rb
+++ app/models/sap/csks.rb
@@ -1,3 +1,9 @@
 class Sap::Csks < ActiveRecord::Base
-  self.primary_keys = :kostl, :datbi
+  self.primary_keys = :kostl
+
+  def self.dept_list
+    sql = "select min(kostl) kostl, kostl||'-'||ktext ktext from sap_csks group by kostl||'-'||ktext order by kostl"
+    Sap::Csks.find_by_sql(sql)
+  end
+
 end
